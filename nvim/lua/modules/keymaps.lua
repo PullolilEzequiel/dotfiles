dc8f367e83dc9b8edd26cd024374cc5c96b517b2
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2025-09-21 18:08:36 -0300

    added telescope configs

--- nvim/lua/modules/keymaps.lua
+++ nvim/lua/modules/keymaps.lua
@@ -25,4 +25,10 @@ vim.keymap.set("n", "<C-9>",":tabn 9<CR>", {noremap=true, silent=true})
 -- hotreload nvim
 vim.api.nvim_set_keymap("n", "<leader>r", ":source %<CR>", { noremap = true, silent = true })
 
+-- telescope 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
